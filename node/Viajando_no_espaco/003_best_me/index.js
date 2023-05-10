5b587852571aa877357da7e208e04d5155b2e60e
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2023-05-09 23:53:52 -0300

    ADD: BestMe end

--- node/Viajando_no_espaco/003_best_me/index.js
+++ node/Viajando_no_espaco/003_best_me/index.js
@@ -6,7 +6,7 @@ const questions = [
 ];
 
 const ask = (index = 0) => {
-  process.stdout.write("\n\n" + questions[index]);
+  process.stdout.write("\n\n" + questions[index] + " > ");
 };
 
 ask();
@@ -17,13 +17,25 @@ process.stdin.on("data", (data) => {
   if (answers.length < questions.length) {
     ask(answers.length);
   } else {
-    let result = "";
-
-    questions.forEach((question, index) => {
-      result += `${question}: ${answers[index]}\n`;
-    });
-
-    process.stdout.write(result);
     process.exit();
   }
+});
+
+process.on("exit", () => {
+  console.log(`
+    Bacana Hemerson!
+
+    O que você aprendeu hoje foi:
+    ${answers[0]}
+
+    O que te aborreceu e você poderia melhorar foi:
+    ${answers[1]}
+
+    O que te deixou feliz hoje:
+    ${answers[2]}
+
+    Você ajudou ${answers[3]} pessoas hoje!!
+
+    Volte amanhã para novas reflexões!
+   `);
 });
